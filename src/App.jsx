3e6db5fc57a38bdcf6a6a9de8573e4a5e8463ae3
TamaraMMT <tamaramolinast@gmail.com>
2023-12-11 17:00:48 -0300

    added navbar, footer and styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div>
         <BrowserRouter>
+          <Navbar />
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
             </Routes>
           </div>
+          <Footer />
         </BrowserRouter>
       </div>
     </>
