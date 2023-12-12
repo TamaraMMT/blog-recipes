af3fc00638d11666c78e7bdaee0baccd6463f9e3
TamaraMMT <tamaramolinast@gmail.com>
2023-12-12 20:37:34 -0300

    added auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+import { AuthProvider } from "./context/AuthContext";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -11,21 +17,38 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user == undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p className="load">Loading...</p>;
+  }
+
   return (
     <>
       <div>
-        <BrowserRouter>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <AuthProvider value={{ user }}>
+          <BrowserRouter>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
       </div>
     </>
   );
