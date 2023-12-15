833e14744319ee2f2356b780a893c156a3f0ffd1
TamaraMMT <tamaramolinast@gmail.com>
2023-12-14 21:07:22 -0300

    added content page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
+import { useAuthValue } from "../../context/AuthContext";
 
 const About = () => {
+  const { user } = useAuthValue();
   return (
-    <div>
+    <div className={styles.about}>
       <h1>About</h1>
+      <p>
+        This project consists of a blog made with React on the front-end and
+        Firebase on the back-end.
+      </p>
+      {user && (
+        <Link to="/register" className="btn-outline">
+          New post
+        </Link>
+      )}
+      {!user && (
+        <Link to="/register" className="btn-outline">
+          Create an account
+        </Link>
+      )}
     </div>
   );
 };
