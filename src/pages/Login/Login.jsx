725830c3ccf496e0807708abcdee374a948977bc
TamaraMMT <tamaramolinast@gmail.com>
2023-12-14 21:20:29 -0300

    added login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,76 @@
 import styles from "./Login.module.css";
 
+import { Link } from "react-router-dom";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import { useState, useEffect } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { login, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await login(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+      setPassword("");
+    }
+  }, [authError]);
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className={styles.login}>
+      <h1>Sign in to a Blog Recipes</h1>
+      <p>
+        Access your account. Don't have one?
+        <Link to="/register">{"Register."}</Link>
+      </p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Sign in</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Wait...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   );
 };
