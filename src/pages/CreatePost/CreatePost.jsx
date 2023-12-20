8f7a1e31771f12f21cf281eec8b3157c6dba9923
TamaraMMT <tamaramolinast@gmail.com>
2023-12-20 06:39:32 -0300

    insert document, errors, check values...

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,113 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useinsertDocument";
+import { useNavigate } from "react-router-dom";
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    /* validate images */
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("The image must be a URL");
+    }
+
+    /* Array for tags created */
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    /* check all values  */
+    if (!title || !image || !tags || !body) {
+      setFormError("Please fill in all fields");
+    }
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+    /* redirect to home page */
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Create post</h1>
+    <div className={styles.create_post}>
+      <h2>Create a Post</h2>
+      <p>Write about anything you like and share your knowledge!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Title:</span>
+          <input
+            type="text"
+            name="text"
+            required
+            placeholder="Think of a great title..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Image URL:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insert an image representing your post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Content:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insert the content of the post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insert tags separated by commas"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        {!response.loading && <button className="btn">Create Post!</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            wait...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   );
 };
