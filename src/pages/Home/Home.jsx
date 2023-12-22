cbcf6ff5693e24ae8b5528981a2908d0e2bdcc52
TamaraMMT <tamaramolinast@gmail.com>
2023-12-22 11:37:56 -0300

    home page, search button,

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,37 @@
 import styles from "./Home.module.css";
 
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h1>Home</h1>
+      <h2>See our most frequent posts</h2>
+      <form className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="or search for tags...."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className={styles.btn_search}>Search</button>
+      </form>
+      <div>
+        <h2>Posts</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.nopost}>
+            <p>No posts found</p>
+            <Link to="/posts/create" className="btn">
+              Create a new post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
