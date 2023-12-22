f82a4e25e5a21bd9c910d9c0e96a353b3ec76cd0
TamaraMMT <tamaramolinast@gmail.com>
2023-12-22 18:05:36 -0300

    added post detail components and fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import styles from "./Home.module.css";
 
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,15 +25,15 @@ const Home = () => {
         <button className={styles.btn_search}>Search</button>
       </form>
       <div>
-        <h2>Posts</h2>
         {posts && posts.length === 0 && (
-          <div className={styles.nopost}>
+          <div className={styles.noposts}>
             <p>No posts found</p>
             <Link to="/posts/create" className="btn">
               Create a new post
             </Link>
           </div>
         )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
     </div>
   );
